cbfe73be45ab814f5615a1125e4b5e98b1056835
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2025-04-08 17:17:40 +0700

    add basePage configuration and sample login page

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,39 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class LoginPage extends BasePage {
+
+    // Locator
+    private By txtUsername = By.xpath("//input[@id='user-name']");
+    private By txtPassword = By.xpath("//input[@id='password']");
+    private By btnLogin = By.xpath("//input[@id='login-button']");
+    private By divLogo = By.xpath("//div[@class='login_logo']");
+    private By imgMascot = By.xpath("//img[@class='bot_column']");
+    private By lblError = By.xpath("//h3[@data-test='error']");
+
+    // Constructor
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    // Actions
+    public void verifyPageLoaded() {
+        verifyElementsAreVisible(txtPassword, txtUsername, divLogo, btnLogin, imgMascot);
+    }
+
+    public void fillUsername(String keyword) {
+        type(txtUsername, keyword);
+    }
+
+    public void fillPassword(String keyword) {
+        type(txtPassword, keyword);
+    }
+
+    public void clickLoginButton() {
+        click(btnLogin);
+    }
+}
