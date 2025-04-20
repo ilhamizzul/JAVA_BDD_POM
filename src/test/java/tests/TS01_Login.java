49d9c877f6cfdc498bec4cebcb56eaaf7bb56e3a
ilhamizzul <ilhamizzul@gmail.com>
2025-04-20 12:37:29 +0700

    Refactor test imports and annotations for TestNG compatibility
    Replaced JUnit annotations with TestNG annotations in `TestBase` to ensure consistency with the project's testing framework. Removed unused imports in both `TestBase` and `TS01_Login` for cleaner and more maintainable code.

--- src/test/java/tests/TS01_Login.java
+++ src/test/java/tests/TS01_Login.java
@@ -1,10 +1,5 @@
 package tests;
 
-import config.WebDriverConfig;
-import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.InventoryPage;
 import pages.LoginPage;
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,13 +1,9 @@
 package tests;
 
 import config.WebDriverConfig;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 
 public class TestBase {
     public WebDriver driver;
