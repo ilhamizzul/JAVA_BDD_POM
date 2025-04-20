933315b75a661a96c0b9457f63f89a3fe7f16731
ilhamizzul <ilhamizzul@gmail.com>
2025-04-20 13:09:41 +0700

    Add new negative login test cases and rename an existing test
    Renamed the TS0102_LoginFailed method for better clarity and added new test cases for various failed login scenarios, including empty username, empty password, both fields empty, and locked-out user. These changes enhance test coverage and improve error handling verification.

--- src/test/java/tests/TS01_Login.java
+++ src/test/java/tests/TS01_Login.java
@@ -17,12 +17,48 @@ public class TS01_Login extends TestBase {
     }
 
     @Test
-    public void TS0102_LoginFailed() {
+    public void TS0102_LoginFailedInvalidUsernameAndPassword() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.navigateToLoginPage();
         loginPage.verifyPageLoaded();
         loginPage.login("random", "yhedujiks");
         loginPage.verifyErrorMessage("Username and password do not match any user in this service");
+    }
+
+    @Test
+    public void TS0103_LoginFailedEmptyUsername() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.navigateToLoginPage();
+        loginPage.verifyPageLoaded();
+        loginPage.login("", "yhedujiks");
+        loginPage.verifyErrorMessage("Username is required");
+    }
+
+    @Test
+    public void TS0104_LoginFailedEmptyPassword() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.navigateToLoginPage();
+        loginPage.verifyPageLoaded();
+        loginPage.login("random", "");
+        loginPage.verifyErrorMessage("Password is required");
+    }
+
+    @Test
+    public void TS0105_LoginFailedEmptyUsernameAndPassword() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.navigateToLoginPage();
+        loginPage.verifyPageLoaded();
+        loginPage.login("", "");
+        loginPage.verifyErrorMessage("Username is required");
+    }
+
+    @Test
+    public void TS0106_LoginFailedLockedOutUser() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.navigateToLoginPage();
+        loginPage.verifyPageLoaded();
+        loginPage.login("locked_out_user", "secret_sauce");
+        loginPage.verifyErrorMessage("Sorry, this user has been locked out.");
     }
 
 }
